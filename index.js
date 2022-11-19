73628cf0714b2f7a5c8e2bcb7f48e497805f2784
Ayobami Ajetunmobi <ajetunmobiayobami19@gmail.com>
2022-11-19 16:11:33 +0100

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const session = require('express-session')
 require('dotenv').config()
+const connectDB = require('./db/connectDb')
 
 app.set('view engine','ejs')
 
@@ -10,3 +11,16 @@ app.use(session({
     saveUninitialized:true,
     secret:process.env.session_secret
 }))
+
+var port = process.env.PORT || 3000
+
+const start = async () =>{
+  try {
+     await config(process.env.Mongo_Url)
+     app.listen(port)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
